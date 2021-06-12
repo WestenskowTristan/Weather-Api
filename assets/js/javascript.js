99b9ebfe28ab634524d654637040dacc6b9713f3
tristan <westenskowtristan122@gmail.com>
2021-06-11 23:17:50 -0600

    added five day forcast cards and funcionality

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,7 +2,7 @@ const WEATHER_API_KEY = "bf3afd387026477c80f7f7186fd25aaf";
 
 const searchForLocation = async (cityName) => {
   let forcastData;
-  let uvIndex;
+  let oneCallData;
   const forcastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${WEATHER_API_KEY}`;
 
   await fetch(forcastApiUrl)
@@ -10,7 +10,6 @@ const searchForLocation = async (cityName) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
       forcastData = data;
     })
     .catch((err) => {
@@ -19,25 +18,101 @@ const searchForLocation = async (cityName) => {
     });
 
   const { lat, lon } = forcastData.city.coord;
-  const oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}`;
+  const oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={}&appid=${WEATHER_API_KEY}`;
 
   await fetch(oneCallApiUrl)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data.current.uvi);
-      uvIndex = data.current.uvi;
+      oneCallData = data;
     })
     .catch((err) => {
-      console.log(`There was a problem fetching data [${serr.message}]`);
+      console.log(`There was a problem fetching data [${err.message}]`);
       throw new Error(err.message);
     });
+
+  renderGeneralCityInfo(forcastData, oneCallData);
+  renderFiveDayForcast(oneCallData);
 };
 
-const searchButton = document.getElementById("forcast-search-button");
+const renderGeneralCityInfo = (forcastData, oneCallData) => {
+  const { city } = forcastData;
+  const { temp, wind_speed, humidity, uvi, weather } = oneCallData.current;
 
-searchButton.addEventListener("click", () => {
-  const searchInputValue = document.getElementById("search-input").value;
+  const date = new Date();
+  const dateDay = date.getDate();
+  const dateMonth = date.getMonth() + 1;
+  const dateYear = date.getFullYear();
+  const formattedDate = `${dateMonth}/${dateDay}/${dateYear}`;
+  const kelvinToFahrenheit = (temp - 273.15) * (9 / 5) + 32;
+
+  const getUviClass = (uvIndex) => {
+    if (uvIndex < 2) return "low";
+    if (uvIndex < 5) return "moderate";
+    if (uvIndex < 7) return "high";
+    if (uvIndex < 10) return "very high";
+    if (uvIndex > 11) return "extreme";
+  };
+
+  const renderedWindSpeed = `Wind ${wind_speed}`;
+  const renderedHumidity = `Humidity ${humidity}`;
+  const renderedUvIndex = `UV Index <span class="uvi-pill ${getUviClass(
+    uvi
+  )}">${uvi}</span>`;
+  const renderedCityName = `${city.name} (${formattedDate})`;
+  const renderedTemp = `Temp  ${Math.round(kelvinToFahrenheit)} °F`;
+
+  const generalCityWeatherInfo = `
+    <h2>${renderedCityName}</h2>
+    <h5>${renderedTemp}</h5>
+    <h5>${renderedWindSpeed}</h5>
+    <h5>${renderedHumidity} %</h5>
+    <h5>${renderedUvIndex}</h5>
+  `;
+
+  $("#general-city-weather-info").empty().html(generalCityWeatherInfo);
+};
+
+const renderFiveDayForcast = (oneCallData) => {
+  console.log(oneCallData);
+  const { daily } = oneCallData;
+
+  const renderedForcastData = daily.map((item, i) => {
+    const { humidity, temp, wind_speed, dt } = item;
+
+    const date = new Date(dt * 1000);
+    const dateDay = date.getDate();
+    const dateMonth = date.getMonth() + 1;
+    const dateYear = date.getFullYear();
+    const formattedDate = `${dateMonth}/${dateDay}/${dateYear}`;
+
+    const renderedWindSpeed = `Wind ${wind_speed}`;
+    const renderedHumidity = `Humidity ${humidity}`;
+    const kelvinToFahrenheit = (temp.day - 273.15) * (9 / 5) + 32;
+    const renderedTemp = `Temp  ${Math.round(kelvinToFahrenheit)} °F`;
+
+    if (3 > i) {
+      return "";
+    }
+
+    return `
+      <div class="forcast-data-card">
+        <h3>${formattedDate}</h3>
+        <h3>${renderedTemp}</h3>
+        <h3>${renderedWindSpeed}</h3>
+        <h3>${renderedHumidity}</h3>
+        
+      </div>
+    `;
+  });
+
+  $(`#forcast`).empty().html(renderedForcastData);
+};
+
+const searchButton = $("#forcast-search-button");
+
+searchButton.on("click", () => {
+  const searchInputValue = $("#search-input").val() || "";
   searchForLocation(searchInputValue);
 });
