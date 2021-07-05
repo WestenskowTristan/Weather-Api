9b4ac4b656a7e330ac6a1a5ecad49915260758bb
tristan <westenskowtristan122@gmail.com>
2021-07-04 19:20:00 -0600

    refactored code

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,115 +1,95 @@
-const WEATHER_API_KEY = "bf3afd387026477c80f7f7186fd25aaf";
-
-const searchForLocation = async (cityName) => {
-  let forcastData;
-  let oneCallData;
-  const forcastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${WEATHER_API_KEY}`;
-
-  await fetch(forcastApiUrl)
-    .then((res) => {
-      return res.json();
+const serach = async (cityName) => {
+  let forcast;
+  let oneCall;
+  // api key
+  var key = "bf3afd387026477c80f7f7186fd25aaf";
+  // api fetch to get forcast
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`
+  )
+    .then((response) => {
+      return response.json();
     })
     .then((data) => {
-      forcastData = data;
+      forcast = data;
     })
     .catch((err) => {
       console.log(`There was a problem fetching data [${err.message}]`);
       throw new Error(err.message);
     });
-
-  const { lat, lon } = forcastData.city.coord;
-  const oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={}&appid=${WEATHER_API_KEY}`;
-
-  await fetch(oneCallApiUrl)
-    .then((res) => {
-      return res.json();
+  const { lat, lon } = forcast?.city?.coord;
+  // api fetch to get city info
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={}&appid=${key}`
+  )
+    .then((response) => {
+      return response.json();
     })
     .then((data) => {
-      oneCallData = data;
+      oneCall = data;
     })
     .catch((err) => {
       console.log(`There was a problem fetching data [${err.message}]`);
       throw new Error(err.message);
     });
-
-  renderGeneralCityInfo(forcastData, oneCallData);
-  renderFiveDayForcast(oneCallData);
+  // renders five day forcast
+  fiveDayForcast(oneCall);
+  // renders city info
+  cityInfo(forcast, oneCall);
 };
-
-const renderGeneralCityInfo = (forcastData, oneCallData) => {
-  const { city } = forcastData;
-  const { temp, wind_speed, humidity, uvi } = oneCallData.current;
-
-  const date = new Date();
-  const dateDay = date.getDate();
-  const dateMonth = date.getMonth() + 1;
-  const dateYear = date.getFullYear();
-  const formattedDate = `${dateMonth}/${dateDay}/${dateYear}`;
-  const kelvinToFahrenheit = (temp - 273.15) * (9 / 5) + 32;
-
-  const getUviClass = (uvIndex) => {
-    if (uvIndex < 2) return "low";
-    if (uvIndex < 5) return "moderate";
-    if (uvIndex < 7) return "high";
-    if (uvIndex < 10) return "very high";
-    if (uvIndex > 11) return "extreme";
+const cityInfo = (forcast, oneCall) => {
+  let { city } = forcast;
+  let { temp, wind_speed, humidity, uvi } = oneCall.current;
+  // creates date format
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  const uviClass = (i) => {
+    if (i < 2) return "low";
+    if (i < 5) return "moderate";
+    if (i < 7) return "high";
+    if (i < 10) return "very high";
+    if (i > 11) return "extreme";
   };
-
-  const renderedWindSpeed = `Wind ${wind_speed}`;
-  const renderedHumidity = `Humidity ${humidity}`;
-  const renderedUvIndex = `UV Index <span class="uvi-pill ${getUviClass(
-    uvi
-  )}">${uvi}</span>`;
-  const renderedCityName = `${city.name} (${formattedDate})`;
-  const renderedTemp = `Temp  ${Math.round(kelvinToFahrenheit)} °F`;
-
-  const generalCityWeatherInfo = `
-    <h2>${renderedCityName}</h2>
-    <h5>${renderedTemp}</h5>
-    <h5>${renderedWindSpeed}</h5>
-    <h5>${renderedHumidity} %</h5>
-    <h5>${renderedUvIndex}</h5>
+  // elements to show weather info
+  const weatherInfo = `
+    <h2>${city.name} (${month}/${day}/${year})</h2>
+    <h5>Temp  ${Math.round((temp - 273.15) * (9 / 5) + 32)} °F</h5>
+    <h5>Wind ${wind_speed}</h5>
+    <h5>Humidity ${humidity} %</h5>
+    <h5>UV Index <span class="uvi-pill ${uviClass(uvi)}">${uvi}</span></h5>
   `;
-
-  $("#general-city-weather-info").empty().html(generalCityWeatherInfo);
+  // render weather info in container
+  $("#weather-info").empty().html(weatherInfo);
 };
-
-const renderFiveDayForcast = (oneCallData) => {
-  const { daily } = oneCallData;
-
-  const renderedForcastData = daily.map((item, i) => {
-    const { humidity, temp, wind_speed, dt } = item;
-
-    const date = new Date(dt * 1000);
-    const dateDay = date.getDate();
-    const dateMonth = date.getMonth() + 1;
-    const dateYear = date.getFullYear();
-    const formattedDate = `${dateMonth}/${dateDay}/${dateYear}`;
-
-    const renderedWindSpeed = `Wind ${wind_speed}`;
-    const renderedHumidity = `Humidity ${humidity}`;
-    const kelvinToFahrenheit = (temp.day - 273.15) * (9 / 5) + 32;
-    const renderedTemp = `Temp  ${Math.round(kelvinToFahrenheit)} °F`;
-
+const fiveDayForcast = (oneCall) => {
+  const { daily } = oneCall;
+  // create forcast elements
+  const forcast = daily.map((item, i) => {
+    let { humidity, temp, wind_speed, dt } = item;
+    let date = new Date(dt * 1000);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    // show every forcast day except current day and after fifth day
     if (i > 5 || i === 0) {
       return "";
     }
-
     return `
       <div class="forcast-data-card">
-        <h3>${formattedDate}</h3>
-        <h3>${renderedTemp}</h3>
-        <h3>${renderedWindSpeed}</h3>
-        <h3>${renderedHumidity}</h3>
-        
+        <h3>${month}/${day}/${year}</h3>
+        <h3> Temp  ${Math.round((temp.day - 273.15) * (9 / 5) + 32)} °F</h3>
+        <h3> Wind ${wind_speed}</h3>
+        <h3> Humidity ${humidity}</h3>
       </div>
     `;
   });
-
-  $(`#forcast`).empty().html(renderedForcastData);
+  // renders forcast elements into container
+  $(`#forcast`).empty().html(forcast);
 };
-
-const localStorageUtil = (action, value) => {
+// saves recent searches to local storage
+const utility = (action, value) => {
   if (action === "set") {
     const searches = JSON.parse(localStorage.getItem("searches") ?? "[]");
     searches.push(value);
@@ -118,35 +98,31 @@ const localStorageUtil = (action, value) => {
     return JSON.parse(localStorage.getItem("searches") ?? "[]");
   }
 };
-
-const renderSaveSearches = async () => {
-  const searches = localStorageUtil("get");
-  const renderedSearches = searches.map((search, i) => {
-    const trimmedSearch = search.replace(/ /g, "-");
+// renders those saved searches and makes them a clickable button
+const savedSearches = () => {
+  const renderedSearches = utility("get").map((search, i) => {
     return `
       <button id="${
-        trimmedSearch + i
-      }" class="recent-search-button">${search} </button>
+        search.replace(/ /g, "-") + i
+      }" class="recent-search">${search} </button>
     `;
   });
-  await $("#recent-city").empty().html(renderedSearches);
-
-  searches.map((search, i) => {
-    const trimmedSearch = search.replace(/ /g, "-");
-    $(`#${trimmedSearch}${i}`).click(() => {
-      searchForLocation(search);
+  $("#recent").empty().html(renderedSearches);
+  utility("get").map((search, i) => {
+    $(`#${search.replace(/ /g, "-")}${i}`).click(() => {
+      serach(search);
       $("#search-input").val(search);
     });
   });
 };
-
-const searchButton = $("#forcast-search-button");
-
+// searches for city typed into search bar
+const searchButton = $("#search-button");
 searchButton.on("click", () => {
-  const searchInputValue = $("#search-input").val() || "";
-  if (searchInputValue !== "") {
-    searchForLocation(searchInputValue);
-    localStorageUtil("set", searchInputValue);
-    renderSaveSearches();
+  if ($("#search-input").val() || "" !== "") {
+    serach($("#search-input").val() || "");
+    utility("set", $("#search-input").val() || "");
+    savedSearches();
+  } else {
+    // do nothing!
   }
 });
